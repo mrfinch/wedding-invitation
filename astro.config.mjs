4d5619a79f74f5476d740deee2b498d5831bdcab
Saurabh Patel <saurabh.finch@gmail.com>
2026-03-11 22:41:23 +0530

    github pages integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mrfinch.github.io',
+  base: '/wedding-invitation',
   vite: {
     plugins: [tailwindcss()]
   }
